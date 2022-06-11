a6b0cef606ce05ec6b00bfc83dc02e812982a5c9
Bakarray <magnificentzeem@gmail.com>
2022-06-11 21:38:56 +0000

    can't use %c for integers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,16 +14,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lstDgt = n%10;
+	lstDgt = n % 10;
 
 	if (lstDgt > 5)
-		printf("Last digit of %d is %c and is greater than 5\n", n, lstDgt);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstDgt);
 
 	else if (lstDgt == 0)
-		printf("Last digit of %d is %c and is 0\n", n, lstDgt);
+		printf("Last digit of %d is %d and is 0\n", n, lstDgt);
 
 	else
-		printf("Last digit of %d is %c and is less than 6 and not 0\n", n, lstDgt);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstDgt);
 
 	return (0);
 }
