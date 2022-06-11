665e57f656532c3c1d8371a8126967d0d3120966
Bakarray <magnificentzeem@gmail.com>
2022-06-11 21:14:25 +0000

    fix if statement typo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
 /**
  * main - Entry point
  *
- * Return (0) success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n < 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
