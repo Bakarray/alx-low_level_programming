c6c23682cd782bf25ae471848ed8b7876b242c77
Bakarray <magnificentzeem@gmail.com>
2022-06-12 15:03:10 +0000

    solved 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int start = 0;
+
+	for (; start <= 10; start++)
+	{
+		printf("%d", start);
+	}
+	printf("\n");
+
+	return (0);
+}
