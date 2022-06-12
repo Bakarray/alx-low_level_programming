f7383808917c56aab7eef43ffa437274dd0c60db
Bakarray <magnificentzeem@gmail.com>
2022-06-12 22:11:13 +0000

    tried adding it to a character

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			putchar(i / 10);
-			putchar(i % 10);
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
 			putchar(' ');
-			putchar(j / 10);
-			putchar(j % 10);
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
 
 			if (i == 98 && j == 99)
 				break;
