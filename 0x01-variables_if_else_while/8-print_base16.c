87601a02ad6cba383c89eb0aebc7ad937ea6f4b1
Bakarray <magnificentzeem@gmail.com>
2022-06-12 21:02:16 +0000

    solved 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+	char alpha;
+
+	for (digit = '0'; digit <= '9'; digit++)
+		putchar(digit);
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+
+	return (0);
+}
