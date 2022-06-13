976a9c44d11c40b419179f1d5b3a9a4fcd67fa32
Bakarray <magnificentzeem@gmail.com>
2022-06-13 19:58:37 +0000

    retyped

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,17 +4,17 @@
  * _isalpha - checks if input is an alphabet
  * @c: The character to be checked
  *
- * Return: 1 (lowercase), 0(uppercase)
+ * Return: 1 (alphabet), 0 (not alphabet)
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-        if (c < 123 && c > 96 || c < 91 && c > 64)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if ((c < 123 && c > 96) || (c < 91 && c > 64))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
