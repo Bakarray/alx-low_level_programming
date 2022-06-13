5315fb8e28bbe7264120889ef220394df3eaff28
Bakarray <magnificentzeem@gmail.com>
2022-06-13 22:50:46 +0000

    make code work for single digit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,7 +9,7 @@
 
 void print_to_98(int num)
 {
-	if (num <= 98)
+	if (num <= 98 && num > 9)
 	{
 		for (; num <= 98; num++)
 		{
@@ -21,13 +21,18 @@ void print_to_98(int num)
 			_putchar(',');
 			_putchar(' ');
 		}
-		_putchar('\n');
+	}
+	else if (num < 10)
+	{
+		_putchar((num % 10) + '0');
+		_putchar(',');
+		_putchar(' ');
 	}
 	else
 	{
 		for (; num >= 98; num--)
 		{
-			if (num > 99)
+			if (num > 99 || num < -99)
 			{
 				_putchar((num / 100) + '0');
 				_putchar(((num / 10) % 10) + '0');
@@ -44,6 +49,6 @@ void print_to_98(int num)
 			_putchar(',');
 			_putchar(' ');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
