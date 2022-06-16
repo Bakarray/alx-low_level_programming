bd267ae2b5e9e59bf8b330d611cf76ee94cc002f
Bakarray <magnificentzeem@gmail.com>
2022-06-16 22:04:00 +0000

    print the first 98 only

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,19 +11,18 @@ int main(void)
 	int i;
 
 	long double fibonacci;
-
 	long double fib1 = 1;
 	long double fib2 = 2;
 
 	printf("%.0Lf, ", fib1);
 	printf("%.0Lf, ", fib2);
 
-	for (i = 3; i <= 98; i++)
+	for (i = 3; i <= 97; i++)
 	{
 		fibonacci = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = fibonacci;
-		if (i == 98)
+		if (i == 97)
 			printf("%.0Lf\n", fibonacci);
 		else
 			printf("%.0Lf, ", fibonacci);
