212bab7bbb62593186339e14ee31c368a26c49b0
Bakarray <magnificentzeem@gmail.com>
2022-06-16 22:08:02 +0000

    start your counting from 1, 2...

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,12 +17,12 @@ int main(void)
 	printf("%.0Lf, ", fib1);
 	printf("%.0Lf, ", fib2);
 
-	for (i = 3; i <= 97; i++)
+	for (i = 3; i <= 99; i++)
 	{
 		fibonacci = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = fibonacci;
-		if (i == 97)
+		if (i == 99)
 			printf("%.0Lf\n", fibonacci);
 		else
 			printf("%.0Lf, ", fibonacci);
