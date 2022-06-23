1aee4e90d1e7253e7469dd19cb092f59e0df9b85
Bakarray <magnificentzeem@gmail.com>
2022-06-23 13:18:28 +0000

    solved 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Counts the length of a string
+ * @s: A pointer to the string to be counted
+ *
+ * Return: The length of the string at s
+ */
+int _strlen_recursion(char *s)
+{
+	int count;
+
+	if (*s)
+	{
+		count = 1 + _strlen_recursion(s + 1);
+	}
+	else
+		count = 0;
+	
+	return (count);
+}
