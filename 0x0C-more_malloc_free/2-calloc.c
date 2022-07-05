55596424271c1799111a5977d9af57d562439dc3
Bakarray <magnificentzeem@gmail.com>
2022-07-05 20:46:32 +0000

    solved 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Function allocates memory for an array using malloc
+ * @nmemb: the number of elements in the array
+ * @size: the size of each element in the array
+ *
+ * Return: A pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *space;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	space = malloc(size * nmemb);
+	if (space == NULL)
+		return (NULL);
+
+	for (i = 0; i <= nmemb; i++)
+		space[i] = 0;
+
+	return (space);
+}
