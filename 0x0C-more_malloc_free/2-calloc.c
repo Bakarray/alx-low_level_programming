acea23115829a8d8e6422961dcfeb6d9a156a9fd
Bakarray <magnificentzeem@gmail.com>
2022-07-05 20:54:09 +0000

    used *(s+1)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,27 +1,26 @@
+#include "holberton.h"
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * _calloc - Function allocates memory for an array using malloc
- * @nmemb: the number of elements in the array
- * @size: the size of each element in the array
- *
- * Return: A pointer to the allocated memory
+ * _calloc - A function that allocates memory for an array using malloc
+ * @nmemb: number of memory spaces
+ * @size: size of nmemb
+ * Return: returns pointer to allocated space, or NULL on failure
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	char *space;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	space = malloc(size * nmemb);
+	space = malloc(nmemb * size);
 	if (space == NULL)
 		return (NULL);
 
-	for (i = 0; i <= nmemb; i++)
-		space[i] = 0;
+	for (; i < nmemb * size; i++)
+		*(space + i) = 0;
 
 	return (space);
 }
