93b5eb72f66cc27a613e55827bdf5c59e0d7cf92
Bakarray <magnificentzeem@gmail.com>
2022-07-11 15:25:53 +0000

    added description for the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - Structure for a dog
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: data structure for a dog
+ */
 struct dog
 {
 	char *name;
